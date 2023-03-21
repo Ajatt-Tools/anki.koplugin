e4f6e009fba48805e9fe084440ebcb876cafd227
nairyo <nairyo@cock.li>
2023-03-21 17:37:22 +0100

    Expand tabs to spaces, following KOreader code style

--- forvo.lua
+++ forvo.lua
@@ -10,85 +10,85 @@ local socket = require("socket")
 local ltn12 = require("ltn12")
 
 local function GET(url)
-	local sink = {}
-	local request = {
-		url = url,
-		method = "GET",
-		headers = {
-			["Content-Type"] = "application/json"
-		},
-		sink = ltn12.sink.table(sink),
-	}
-	local code, _, status = socket.skip(1, http.request(request))
-	if code == 200 then
-		return table.concat(sink)
-	end
+    local sink = {}
+    local request = {
+        url = url,
+        method = "GET",
+        headers = {
+            ["Content-Type"] = "application/json"
+        },
+        sink = ltn12.sink.table(sink),
+    }
+    local code, _, status = socket.skip(1, http.request(request))
+    if code == 200 then
+        return table.concat(sink)
+    end
 end
 
 -- http://lua-users.org/wiki/BaseSixtyFour
 -- character table string
 local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 local function base64e(data)
-	return ((data:gsub('.', function(x) 
-		local r,b='',x:byte()
-		for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
-		return r;
-	end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
-		if (#x < 6) then return '' end
-		local c=0
-		for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
-		return b:sub(c+1,c+1)
-	end)..({ '', '==', '=' })[#data%3+1])
+    return ((data:gsub('.', function(x) 
+        local r,b='',x:byte()
+        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c=0
+        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+        return b:sub(c+1,c+1)
+    end)..({ '', '==', '=' })[#data%3+1])
 end
 
 local function base64d(data)
-	data = string.gsub(data, '[^'..b..'=]', '')
-	return (data:gsub('.', function(x)
-		if (x == '=') then return '' end
-		local r,f='',(b:find(x)-1)
-		for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
-		return r;
-	end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-		if (#x ~= 8) then return '' end
-		local c=0
-		for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
-		return string.char(c)
-	end))
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+        return string.char(c)
+    end))
 end
 
 
 local function url_encode(url)
-	-- https://gist.github.com/liukun/f9ce7d6d14fa45fe9b924a3eed5c3d99
-	local char_to_hex = function(c)
-		return string.format("%%%02X", string.byte(c))
-	end
-	if url == nil then
-		return
-	end
-	url = url:gsub("\n", "\r\n")
-	url = url:gsub("([^%w _%%%-%.~])", char_to_hex)
-	url = url:gsub(" ", "+")
-	return url
+    -- https://gist.github.com/liukun/f9ce7d6d14fa45fe9b924a3eed5c3d99
+    local char_to_hex = function(c)
+        return string.format("%%%02X", string.byte(c))
+    end
+    if url == nil then
+        return
+    end
+    url = url:gsub("\n", "\r\n")
+    url = url:gsub("([^%w _%%%-%.~])", char_to_hex)
+    url = url:gsub(" ", "+")
+    return url
 end
 
 local function get_pronunciation_url(word)
-	local forvo_url = string.format('https://forvo.com/search/%s/ja', url_encode(word))
-	local forvo_page = GET(forvo_url)
-	if not forvo_page then
-		return false
-	end
-	local play_params = string.match(forvo_page, "Play%((.-)%);")
+    local forvo_url = string.format('https://forvo.com/search/%s/ja', url_encode(word))
+    local forvo_page = GET(forvo_url)
+    if not forvo_page then
+        return false
+    end
+    local play_params = string.match(forvo_page, "Play%((.-)%);")
 
-	local word_url = nil
-	if play_params then
-		local iter = string.gmatch(play_params, "'(.-)'")
-		local formats = { mp3 = iter(), ogg = iter() }
-		word_url = string.format('https://audio00.forvo.com/%s/%s', "ogg", base64d(formats["ogg"]))
-	end
-	return true, word_url
+    local word_url = nil
+    if play_params then
+        local iter = string.gmatch(play_params, "'(.-)'")
+        local formats = { mp3 = iter(), ogg = iter() }
+        word_url = string.format('https://audio00.forvo.com/%s/%s', "ogg", base64d(formats["ogg"]))
+    end
+    return true, word_url
 end
 
 return {
-	get_pronunciation_url = get_pronunciation_url,
-	base64e = base64e,
+    get_pronunciation_url = get_pronunciation_url,
+    base64e = base64e,
 }
--- main.lua
+++ main.lua
@@ -10,21 +10,21 @@ local Widget = require("ui/widget/widget")
 local util = require("util")
 
 local AnkiWidget = Widget:extend {
-	-- this contains all the user configurable options
-	-- to access them: conf.xxx:get_value()
-	conf = require("ankidefaults"),
+    -- this contains all the user configurable options
+    -- to access them: conf.xxx:get_value()
+    conf = require("ankidefaults"),
 }
 
 function AnkiWidget:show_config_widget(popup_dict)
-	local note_count = #self.anki_connect.local_notes
-	local sync_message = string.format(string.format("Sync (%d) offline note(s)", note_count))
-	self.config_widget = ButtonDialog:new {
-		buttons = {
-			{{ text = sync_message, id = "sync", enabled = note_count > 0, callback = function() self.anki_connect:sync_offline_notes() end }},
-			{{ text = "Add with custom tags", id = "add_custom", callback = function() self.anki_connect:add_note(popup_dict, self.conf.custom_tags:get_value()) end }},
-		}
-	}
-	UIManager:show(self.config_widget)
+    local note_count = #self.anki_connect.local_notes
+    local sync_message = string.format(string.format("Sync (%d) offline note(s)", note_count))
+    self.config_widget = ButtonDialog:new {
+        buttons = {
+            {{ text = sync_message, id = "sync", enabled = note_count > 0, callback = function() self.anki_connect:sync_offline_notes() end }},
+            {{ text = "Add with custom tags", id = "add_custom", callback = function() self.anki_connect:add_note(popup_dict, self.conf.custom_tags:get_value()) end }},
+        }
+    }
+    UIManager:show(self.config_widget)
 end
 
 -- [[
@@ -35,160 +35,160 @@ end
 -- items to the dictionary menu
 -- ]]
 function AnkiWidget:addToMainMenu(menu_items)
-	local settings = {}
-	for _, opt in ipairs(self.conf) do
-		logger.info("AnkiWidget#addToMainMenu(): ", opt.id)
-		settings[#settings+1] = {
-			text = opt.name,
-			keep_menu_open = true,
-			callback = function()
-				if opt.conf_type == "text" then
-					self:build_single_dialog(opt)
-				elseif opt.conf_type == "table" then
-					self:build_multi_dialog(opt)
-				else
-					UIManager:show(InfoMessage:new { text = string.format("Configuration of type %s can only be edited on PC!", opt.conf_type), timeout = 3 })
-				end
-			end
-		}
-	end
-	menu_items.anki_settings = {
-		text = "Anki Settings",
-		sub_item_table = settings,
-	}
+    local settings = {}
+    for _, opt in ipairs(self.conf) do
+        logger.info("AnkiWidget#addToMainMenu(): ", opt.id)
+        settings[#settings+1] = {
+            text = opt.name,
+            keep_menu_open = true,
+            callback = function()
+                if opt.conf_type == "text" then
+                    self:build_single_dialog(opt)
+                elseif opt.conf_type == "table" then
+                    self:build_multi_dialog(opt)
+                else
+                    UIManager:show(InfoMessage:new { text = string.format("Configuration of type %s can only be edited on PC!", opt.conf_type), timeout = 3 })
+                end
+            end
+        }
+    end
+    menu_items.anki_settings = {
+        text = "Anki Settings",
+        sub_item_table = settings,
+    }
 
 end
 
 function AnkiWidget:build_single_dialog(opt)
-	local input_dialog -- saved first so we can reference it in the callbacks
-	input_dialog = InputDialog:new {
-		title = opt.name,
-		input = opt:get_value(),
-		input_hint = opt.name,
-		description = opt.description,
-		buttons = {{
-			{ text = "Cancel", 	id = "cancel", 		callback = function() UIManager:close(input_dialog) end },
-			{ text = "Save", 	id = "save", 		callback = function()
-				opt:update_value(input_dialog:getInputText())
-				UIManager:close(input_dialog)
-			end },
-		}},
-	}
-	UIManager:show(input_dialog)
-	input_dialog:onShowKeyboard()
+    local input_dialog -- saved first so we can reference it in the callbacks
+    input_dialog = InputDialog:new {
+        title = opt.name,
+        input = opt:get_value(),
+        input_hint = opt.name,
+        description = opt.description,
+        buttons = {{
+            { text = "Cancel",  id = "cancel",      callback = function() UIManager:close(input_dialog) end },
+            { text = "Save",    id = "save",        callback = function()
+                opt:update_value(input_dialog:getInputText())
+                UIManager:close(input_dialog)
+            end },
+        }},
+    }
+    UIManager:show(input_dialog)
+    input_dialog:onShowKeyboard()
 end
 
 function AnkiWidget:build_multi_dialog(opt)
-	local fields = {}
-	for k,v in pairs(opt:get_value()) do
-		table.insert(fields, { description = k, text = v })
-	end
+    local fields = {}
+    for k,v in pairs(opt:get_value()) do
+        table.insert(fields, { description = k, text = v })
+    end
 
-	local multi_dialog
-	multi_dialog = MultiInputDialog:new {
-		title = opt.name,
-		description = opt.description,
-		fields = fields,
-		buttons = {{
-			{ text = "Cancel", 	id = "cancel", 		callback = function() UIManager:close(multi_dialog) end },
-			{ text = "Save", 	id = "save", 		callback = function()
-				local new = {}
-				for idx,v in ipairs(multi_dialog:getFields()) do
-					new[fields[idx].description] = v
-				end
-				opt:update_value(new)
-				UIManager:close(multi_dialog)
-			end},
-			}
-		},
-	}
-	UIManager:show(multi_dialog)
-	multi_dialog:onShowKeyboard()
+    local multi_dialog
+    multi_dialog = MultiInputDialog:new {
+        title = opt.name,
+        description = opt.description,
+        fields = fields,
+        buttons = {{
+            { text = "Cancel",  id = "cancel",      callback = function() UIManager:close(multi_dialog) end },
+            { text = "Save",    id = "save",        callback = function()
+                local new = {}
+                for idx,v in ipairs(multi_dialog:getFields()) do
+                    new[fields[idx].description] = v
+                end
+                opt:update_value(new)
+                UIManager:close(multi_dialog)
+            end},
+            }
+        },
+    }
+    UIManager:show(multi_dialog)
+    multi_dialog:onShowKeyboard()
 end
 
 -- This function is called automatically for all tables extending from Widget
 function AnkiWidget:init()
-	-- this contains all the logic for creating anki cards
-	self.anki_connect = require("ankiconnect"):new {
-		conf = self.conf,
-		btn = self.add_to_anki_btn,
-		ui = self.ui, -- AnkiConnect helper class has no access to the UI by default, so add it here
-	}
+    -- this contains all the logic for creating anki cards
+    self.anki_connect = require("ankiconnect"):new {
+        conf = self.conf,
+        btn = self.add_to_anki_btn,
+        ui = self.ui, -- AnkiConnect helper class has no access to the UI by default, so add it here
+    }
 
-	self.ui.menu:registerToMainMenu(self)
-	self:handle_events()
-	-- Insert new button in the popup dictionary to allow adding anki cards
-	DictQuickLookup.tweak_buttons_func = function(popup_dict, buttons)
-		self.add_to_anki_btn = {
-			id = "add_to_anki",
-			text = _("Add to Anki"),
-			font_bold = true,
-			callback = function() self.anki_connect:add_note(popup_dict, {}) end,
-			hold_callback = function() UIManager:show(self:show_config_widget(popup_dict)) end,
-		}
-		table.insert(buttons, 1, { self.add_to_anki_btn })
-	end
+    self.ui.menu:registerToMainMenu(self)
+    self:handle_events()
+    -- Insert new button in the popup dictionary to allow adding anki cards
+    DictQuickLookup.tweak_buttons_func = function(popup_dict, buttons)
+        self.add_to_anki_btn = {
+            id = "add_to_anki",
+            text = _("Add to Anki"),
+            font_bold = true,
+            callback = function() self.anki_connect:add_note(popup_dict, {}) end,
+            hold_callback = function() UIManager:show(self:show_config_widget(popup_dict)) end,
+        }
+        table.insert(buttons, 1, { self.add_to_anki_btn })
+    end
 end
 
 function AnkiWidget:extend_doc_settings()
-	local doc = self.ui.document
-	local _, file = util.splitFilePathName(doc.file)
-	local file_pattern = "^%[([^%]]-)%]_(.-)_%[([^%]]-)%]%.[^%.]+"
-	local f_author, f_title, f_extra = file:match(file_pattern)
-	local file_properties = {
-		title = f_title,
-		author = f_author,
-		description = f_extra,
-	}
-	local document_properties = doc:getProps()
-	local get_prop = function(property)
-		local d_p, f_p = document_properties[property], file_properties[property]
-		local d_len, f_len = d_p and #d_p or 0, f_p and #f_p or 0
-		-- if our custom f_p match is more exact, pick that one
-		-- e.g. for PDF the title is usually the full filename
-		local f_p_more_precise = d_len == 0 or d_len > f_len and f_len ~= 0
-		return f_p_more_precise and f_p or d_p
-	end
-	local metadata = {
-		title = get_prop('title'),
-		author = get_prop('author') or get_prop('authors'),
-		description = get_prop('description'),
-		current_page = function() return self.ui.view.state.page end,
-		pages = doc.info.number_of_pages or "N/A"
-	}
-	local metadata_mt = {
-		__index = function(t, k) return rawget(t, k) or "N/A" end
-	}
-	self.ui.document._anki_metadata = setmetatable(metadata, metadata_mt)
+    local doc = self.ui.document
+    local _, file = util.splitFilePathName(doc.file)
+    local file_pattern = "^%[([^%]]-)%]_(.-)_%[([^%]]-)%]%.[^%.]+"
+    local f_author, f_title, f_extra = file:match(file_pattern)
+    local file_properties = {
+        title = f_title,
+        author = f_author,
+        description = f_extra,
+    }
+    local document_properties = doc:getProps()
+    local get_prop = function(property)
+        local d_p, f_p = document_properties[property], file_properties[property]
+        local d_len, f_len = d_p and #d_p or 0, f_p and #f_p or 0
+        -- if our custom f_p match is more exact, pick that one
+        -- e.g. for PDF the title is usually the full filename
+        local f_p_more_precise = d_len == 0 or d_len > f_len and f_len ~= 0
+        return f_p_more_precise and f_p or d_p
+    end
+    local metadata = {
+        title = get_prop('title'),
+        author = get_prop('author') or get_prop('authors'),
+        description = get_prop('description'),
+        current_page = function() return self.ui.view.state.page end,
+        pages = doc.info.number_of_pages or "N/A"
+    }
+    local metadata_mt = {
+        __index = function(t, k) return rawget(t, k) or "N/A" end
+    }
+    self.ui.document._anki_metadata = setmetatable(metadata, metadata_mt)
 end
 
 function AnkiWidget:handle_events()
-	-- these all return false so that the event goes up the chain, other widgets might wanna react to these events
-	self.onCloseWidget = function()
-		self.anki_connect:save_notes()
-		self.conf:save()
-	end
+    -- these all return false so that the event goes up the chain, other widgets might wanna react to these events
+    self.onCloseWidget = function()
+        self.anki_connect:save_notes()
+        self.conf:save()
+    end
 
-	self.onSuspend = function()
-		self.anki_connect:save_notes()
-		self.conf:save()
-	end
+    self.onSuspend = function()
+        self.anki_connect:save_notes()
+        self.conf:save()
+    end
 
-	self.onResume = function()
-		self.anki_connect:load_notes()
-	end
+    self.onResume = function()
+        self.anki_connect:load_notes()
+    end
 
-	self.onNetworkConnected = function()
-		self.anki_connect.wifi_connected = true
-	end
+    self.onNetworkConnected = function()
+        self.anki_connect.wifi_connected = true
+    end
 
-	self.onNetworkDisconnected = function()
-		self.anki_connect.wifi_connected = false
-	end
+    self.onNetworkDisconnected = function()
+        self.anki_connect.wifi_connected = false
+    end
 
-	self.onReaderReady = function()
-		self:extend_doc_settings()
-	end
+    self.onReaderReady = function()
+        self:extend_doc_settings()
+    end
 end
 
 return AnkiWidget
