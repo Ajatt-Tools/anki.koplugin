a64e89fb752fa66b9ec2c916fa6280ab3c09d970
nairyo <nairyo@cock.li>
2025-08-20 18:49:34 +0200

    update pitch downstep pattern to match special style-digit symbols

--- extensions/EXT_pitch_accent.lua
+++ extensions/EXT_pitch_accent.lua
@@ -17,7 +17,7 @@ this extension extracts the [2] from the definition's headword and stores it as 
     mark_accented = "<span style=\"display:inline-block;position:relative;\"><span style=\"display:inline;\">%s</span><span style=\"border-color:currentColor;display:block;user-select:none;pointer-events:none;position:absolute;top:0.1em;left:0;right:0;height:0;border-top-width:0.1em;border-top-style:solid;\"></span></span>",
     mark_downstep = "<span style=\"display:inline-block;position:relative;padding-right:0.1em;margin-right:0.1em;\"><span style=\"display:inline;\">%s</span><span style=\"border-color:currentColor;display:block;user-select:none;pointer-events:none;position:absolute;top:0.1em;left:0;right:0;height:0;border-top-width:0.1em;border-top-style:solid;right:-0.1em;height:0.4em;border-right-width:0.1em;border-right-style:solid;\"></span></span>",
     unmarked_char = "<span style=\"display:inline-block;position:relative;\"><span style=\"display:inline;\">%s</span><span style=\"border-color:currentColor;\"></span></span>",
-    pitch_downstep_pattern = "(%[([0-9])%])",
+    pitch_downstep_pattern = "([⓪①②③④⑤⑦⑧⑨]|%[([0-9])%])",
 }
 
 function PitchAccent:convert_pitch_to_HTML(accents)
